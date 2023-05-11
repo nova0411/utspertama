24e84d7eaf2a43157a61eb7d8095535e40b1dc9a
nova0411 <novaalvin03@gmail.com>
2023-05-11 17:46:10 +0700

    UTS_PBO_Nova Alvin Utomo_G.111.20.0028

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
+
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+
+        mhs.setNama("Nova Alvin Utomo");
+        mhs.setNim("G.111.20.0028");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
 
     }
 }
